cbd4985425f8e5f4bfec3f4d636d0f12ff9fcebb
Rey Charles Lomongo <115441227+ReyCharles12@users.noreply.github.com>
2022-10-31 20:15:26 +0800

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int num;
+
+    printf("Please enter a positive number: \n");
+    scanf("%d", &num);
+
+    printf("\nValue entered: %d", num);
+
+    return 0;
+}
